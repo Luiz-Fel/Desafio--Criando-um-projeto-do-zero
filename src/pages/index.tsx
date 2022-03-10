2eabc9ab00790a552338c126027087f3c8334a4e
Luiz Felipe <lfsprj@gmail.com>
2022-03-10 20:33:06 -0300

    Added home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { GetStaticProps } from 'next';
-
+import ptBR from 'date-fns/locale/pt-BR';
+import { format } from 'date-fns';
 import { getPrismicClient } from '../services/prismic';
 import Prismic from '@prismicio/client'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -25,10 +28,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home(props) {
+ 
+
+  console.log(posts) 
   return(
-    <div>
-      
+    <div className={styles.main}>
+
+      {posts.map((current) => {
+        return(
+          <div key={current.uid} className={styles.post}>
+            <Link href={'/'}>
+              <a className={styles.title}>
+                <h2>{current.data.title[0].text}</h2>
+              </a>
+            </Link>
+            <Link href={'/'}>
+              <a className={styles.subTitle}>
+                <p>{current.data.subtitle[0].text}</p>
+              </a>
+            </Link>
+            <div className={styles.dateAndAuthor}>
+              <div className={styles.postSubContent}>
+              <p>ícone</p>
+              <p>
+                {
+                format(new Date(Date.parse(current.first_publication_date)), 
+                      'dd MMM yyyy', {locale: ptBR})
+                }
+                </p>
+              </div>
+              <div className={styles.postSubContent}>
+              <p>ícone</p>
+                <p>
+                {
+                  current.data.author[0].text
+                }
+                </p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+      <button className={styles.button} onClick={() => addPosts()}>
+        Carregar mais posts
+      </button>
     </div>
   )
 }
@@ -36,14 +80,17 @@ export default function Home() {
 export const getStaticProps = async () => {
    const prismic = getPrismicClient();
    const postsResponse = await prismic.query
-   ([ Prismic.Predicates.at('document.type', 'post')],
+   ([ Prismic.Predicates.at('document.type', 'posts')],
    {
      fetch: ['Post.Title', 'Post.Content'],
-     pageSize: 10
+     pageSize: 5
    });
+   console.log(postsResponse)
 
   // TODO
   return {
-    props: {}
+    props: {
+      posts: postsResponse
+    }
   }
 };
